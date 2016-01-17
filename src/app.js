35932d9481f0838feba5f912cb5b98e92bfa0bdf
Nick Johnstone <ncwjohnstone@gmail.com>
2016-01-17 20:18:44 +1300

    Add static mega suns, suns and planets

--- src/app.js
+++ src/app.js
@@ -3,8 +3,32 @@ import {a, div, button, input} from '@cycle/dom';
 
 import {Observable} from 'rx';
 
+function megaSun ({x, y}) {
+  return (
+    div('.mega-sun', {style: {top: y, left: x}})
+  );
+}
+
+function sun ({x, y}) {
+  return (
+    div('.sun', {style: {top: y, left: x}})
+  );
+}
+
+function planet ({x, y}) {
+  return (
+    div('.planet', {style: {top: y, left: x}})
+  );
+}
+
 export default function App ({DOM, HTTP}) {
   return {
-    DOM: Observable.just(div('Hi!'))
+    DOM: Observable.just(
+      div('.galaxy', [
+        megaSun({x: '50%', y: '50%'}),
+        sun({x: '50%', y: '70%'}),
+        planet({x: '50%', y: '80%'})
+      ])
+    )
   };
 }
